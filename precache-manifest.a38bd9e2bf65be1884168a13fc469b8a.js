58e52cb7f02f00f1a72f31d1592e742d11f732d8
Karolis Baukas <KarolisBaukas5@gmail.com>
2020-03-29 13:44:57 +0200

    Updates

---
+++ precache-manifest.a38bd9e2bf65be1884168a13fc469b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3317c13e33b303b2baca81b561fde4fa",
+    "url": "/Dice-Game/index.html"
+  },
+  {
+    "revision": "d69ad5e1bca18984ddbe",
+    "url": "/Dice-Game/static/css/main.5ac226a9.chunk.css"
+  },
+  {
+    "revision": "f192fbc29087b061555a",
+    "url": "/Dice-Game/static/js/2.65c2d4a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dice-Game/static/js/2.65c2d4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69ad5e1bca18984ddbe",
+    "url": "/Dice-Game/static/js/main.f83385bd.chunk.js"
+  },
+  {
+    "revision": "bde2529d9ef595a6d9a9",
+    "url": "/Dice-Game/static/js/runtime-main.c686aa0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dice-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
